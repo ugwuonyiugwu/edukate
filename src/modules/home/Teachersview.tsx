16bd8d9c6c2a08717141288e5b748a02ed88c5e2
Your Name <you@example.com>
2026-04-29 17:34:43 +0100

    corrected 2

--- src/modules/home/Teachersview.tsx
+++ src/modules/home/Teachersview.tsx
@@ -84,7 +84,7 @@ export const TeacherView = () => {
                   asChild
                 >
                   <a 
-                    href={`https://wa.me/git${teacher.whatsappNumber.replace(/\D/g, '')}`} 
+                    href={`https://wa.me/${teacher.whatsappNumber}`} 
                     target="_blank" 
                     rel="noreferrer"
                     className="flex items-center gap-2"
